fc3c1f2f31357be33e9cf2eefa314766c6010469
Herman White <phoenixquint@gmail.com>
2019-02-19 00:46:54 -0500

    Updates

---
+++ precache-manifest.e01a77c61f6b09fc225d9314670ad6c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ae900c07830e5d05ff6",
+    "url": "/ffxiv-market-watch/static/css/main.3e373cae.chunk.css"
+  },
+  {
+    "revision": "4ae900c07830e5d05ff6",
+    "url": "/ffxiv-market-watch/static/js/main.4ae900c0.chunk.js"
+  },
+  {
+    "revision": "38d6dc8823ca9bb6ce1e",
+    "url": "/ffxiv-market-watch/static/js/runtime~main.38d6dc88.js"
+  },
+  {
+    "revision": "2e795f6cf42cb447b7e0",
+    "url": "/ffxiv-market-watch/static/js/2.2e795f6c.chunk.js"
+  },
+  {
+    "revision": "8e3ab67b79bb0b3fa48c8f91ebfa2e52",
+    "url": "/ffxiv-market-watch/static/media/mario-coin.8e3ab67b.wav"
+  },
+  {
+    "revision": "e8e60fea5c09ede27765dbffb6cb47d3",
+    "url": "/ffxiv-market-watch/static/media/finalf.e8e60fea.ttf"
+  },
+  {
+    "revision": "e89205fdab6a18400218d0687a4aec47",
+    "url": "/ffxiv-market-watch/index.html"
+  }
+];
